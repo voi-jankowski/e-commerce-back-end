07d0d6cd1dfac6e16e32f1c802e6ff086a0791b7
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-05-12 16:58:29 +1000

    Created get api endpoints for category-routes.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
-
+router.use("/api", apiRoutes);
+// if the route is different from /api, send a message that the route is wrong
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1>");
 });
 
 module.exports = router;
