b33b4562cc8e9b87045ec71c071e7c6b5c6bc46a
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-05-12 17:44:37 +1000

    Created delete api endpoint for product-routes.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,7 +42,7 @@ router.get("/:id", async (req, res) => {
 });
 
 // create new product
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -115,8 +115,20 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: { id: req.params.id },
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
