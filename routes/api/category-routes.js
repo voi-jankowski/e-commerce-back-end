7746b567d7423c2b63d17b34a65285ca9c0f59db
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-05-12 17:12:23 +1000

    Created put  api endpoint for category-routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,8 +43,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
